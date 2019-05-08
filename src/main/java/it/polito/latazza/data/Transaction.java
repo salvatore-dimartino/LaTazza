9257e1b6dbf08a25bbd93355b5af72c698fc6cb2
Salvatore Di Martino <s218584@studenti.polito.it>
2019-05-08 18:42:34 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/Transaction.java
@@ -0,0 +1,35 @@
+package it.polito.latazza.data;
+
+import java.util.Date;
+
+public class Transaction {
+	
+	private Integer ID;	private Date date; 
+	
+	public Transaction(int ID, Date date) {
+		this.ID = ID;
+		this.date = date;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
