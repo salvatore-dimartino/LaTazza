405cf28002898f5e3a847a54d5cc98c1abe1dfff
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-10 12:55:10 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/Recharge.java
@@ -0,0 +1,43 @@
+package it.polito.latazza.data;
+
+import java.util.Date;
+
+public class Recharge extends Transaction {
+	
+	//default serialVersion id
+	private static final long serialVersionUID = 1L;
+	
+	private Integer amount;
+	private Employee employee;
+
+	public Recharge(int ID, Date date, Integer amount, Employee employee) {
+		super(ID, date);
+		this.amount=amount;
+		this.employee=employee;
+		
+		this.WriteObjectToFile(this);
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	// The string format for a recharge is "[datetime] RECHARGE [employee] [amount]".
+	public String getString() {
+		return this.getDate().toString() + " RECHARGE " + this.getEmployee().getName() + " " +
+			   this.getEmployee().getSurname() + " " + this.amount.toString();
+	}
+
+}
