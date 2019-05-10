eec5cde2abf649281caacdebcc8ec039dd25a220
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-10 13:04:51 +0000

    Revert "Merge branch 'Francesco_Dibitonto' into 'Dinamo_coding'"
    This reverts merge request !100

--- src/main/java/it/polito/latazza/data/Transaction.java
+++ src/main/java/it/polito/latazza/data/Transaction.java
@@ -1,25 +1,14 @@
 package it.polito.latazza.data;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.Date;
 
-public class Transaction implements Serializable{
+public class Transaction {
 	
-	//default serialVersion id
-    private static final long serialVersionUID = 1L;
-    
 	private Integer ID;	private Date date; 
-	
-	private static final String filepath=(new File("transactions.txt")).getAbsolutePath();
 	
 	public Transaction(int ID, Date date) {
 		this.ID = ID;
 		this.date = date;
-		
-		this.WriteObjectToFile(this);
 	}
 
 	public int getID() {
@@ -42,18 +31,5 @@ public class Transaction implements Serializable{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	public void WriteObjectToFile(Object serObj) {
-		 
-        try {
-            FileOutputStream fileOut = new FileOutputStream(filepath, true);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(serObj);
-            objectOut.close();
- 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
 
 }
