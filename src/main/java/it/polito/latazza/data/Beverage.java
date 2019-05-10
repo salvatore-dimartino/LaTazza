885e57c07299517a1edd498e71b387c256b566a8
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-10 15:38:06 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/Beverage.java
@@ -0,0 +1,83 @@
+package it.polito.latazza.data;
+
+import org.json.simple.JsonObject;
+
+public class Beverage {
+	
+	private Integer ID;
+	private String name;
+	private Integer price;
+	private Integer quantityPerBox;
+	private Integer availableQuantity;
+	
+	private JsonObject json = new JsonObject();
+	
+	public Beverage(Integer ID, String name, Integer price, Integer quantityPerBox, Integer availableQuantity) {
+		
+		this.ID = ID;
+		this.name = name;
+		this.price = price;
+		this.quantityPerBox = quantityPerBox;
+		this.availableQuantity = availableQuantity;
+		
+		// create beverage json file
+		json.put("ID", ID.toString());  
+		json.put("name", name);
+		json.put("price", price.toString());
+		json.put("quantityPerBox", quantityPerBox.toString());
+		json.put("availableQuantity", availableQuantity.toString());
+		
+	}
+	
+	public JsonObject getJson() {
+		return json;
+	}
+
+	public void setJson(JsonObject json) {
+		this.json = json;
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Integer getQuantityPerBox() {
+		return quantityPerBox;
+	}
+	
+	public Integer getAvailableQuantity() {
+		return availableQuantity;
+	}
+	
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setID(Integer ID) {
+		this.ID = ID;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public void setQuantityPerBox(Integer quantityPerBox) {
+		this.quantityPerBox = quantityPerBox;
+	}
+
+	public void setAvailableQuantity(Integer availableQuantity) {
+		this.availableQuantity = availableQuantity;
+	}
+	
+	
+	
+	
+}
