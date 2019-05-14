66efca36dd5faf13a09e06d04770b1ad83deb021
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-14 10:26:43 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/Transaction.java
@@ -0,0 +1,95 @@
+package it.polito.latazza.data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Transaction {
+	
+	private Integer ID;
+	private Date date; 
+	
+	public Transaction(int ID, Date date) {
+		
+		this.ID = ID;
+		this.date = date;
+		
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public List<String> getAttributes() {
+		
+		List<String> att = new ArrayList<String>();
+		
+		att.add(this.date.toString());
+		
+		return att;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void toJsonTransaction() {
+		
+		JSONParser parser = new JSONParser();
+		JSONObject j_file = new JSONObject();
+		
+		File myfile = new File("Transactions.json");
+		try {
+			myfile.createNewFile();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		
+		try {
+			j_file = (JSONObject) parser.parse(new FileReader("./Transactions.json"));
+						
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (ParseException e) {
+		}	
+		
+		j_file.put(this.ID.toString(), this.getAttributes());
+						
+		// write the json object to the file
+		try (FileWriter file = new FileWriter("./Transactions.json")) {
+			file.write(j_file.toJSONString());
+			file.flush();
+					 
+		} catch (IOException e) {
+			e.printStackTrace();
+	    }
+
+	}
+
+}
