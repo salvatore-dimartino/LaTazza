0e4671342dae2bf4e181bea7140c5519007fb7a5
Salvatore Di Martino <s218584@studenti.polito.it>
2019-05-17 14:08:51 +0000

    Upload New File

---
+++ src/test/java/it/polito/latazza/data/ConsumptionTest.java
@@ -0,0 +1,65 @@
+package it.polito.latazza.data;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import it.polito.latazza.exceptions.BeverageException;
+
+public class ConsumptionTest extends junit.framework.TestCase {
+	
+	Consumption c1, c2;
+	Date d1;
+	Beverage b1;
+	Employee e1;
+		
+		public ConsumptionTest() throws Exception {
+			setUp();
+		}
+		
+		protected void setUp() throws Exception {
+			
+			d1 = new Date();
+			b1 = new Beverage(0, "Caffe", 3000, 10, 0);
+			e1 = new Employee("Ludito", "Arduca", 0);
+			c1 = new Consumption(0, d1, 100, b1, e1, "CASH");
+			
+		}
+		
+		@Test
+		public void testConsumption() throws Exception {
+			
+			Consumption c3;
+			
+			try{
+				c3 = new Consumption(0, d1, 200, b1, e1, "BALANCE");
+			}catch(Exception e) {
+				fail();
+			}
+			
+			assertThrows(Exception.class, () -> {Consumption c4 = new Consumption(0, d1, -200, b1, e1, "BALANCE");});
+			
+			return;
+		}
+		
+		@Test
+		public void testSetQuantity() {
+			try {
+				c1.setQuantity(10);
+			}catch(Exception e) {
+				fail();
+			}
+			
+			assertThrows(Exception.class, () -> {c1.setQuantity(-10);});
+			
+			return;
+		}
+		
+		@Test
+		public void testGetQuantity() {
+			assertTrue(c1.getQuantity() == 100);
+			return;
+		}
+}
