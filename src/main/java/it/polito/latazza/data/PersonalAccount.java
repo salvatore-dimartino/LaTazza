5a4550f24a4612cf3e7732589545f806a62ed1fb
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-17 10:46:16 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/PersonalAccount.java
@@ -0,0 +1,35 @@
+package it.polito.latazza.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PersonalAccount {
+	private Integer balance;
+	private Map<Integer,Transaction> Transactions=new HashMap<Integer,Transaction>();
+	
+	public PersonalAccount(Integer balance) {
+		this.balance=balance;
+	}
+	
+	public Integer getBalance() {
+		return this.balance;
+	}
+	
+	public void setBalance(Integer balance) {
+		this.balance=balance;
+	}
+	
+	public Map<Integer,Transaction> getTransactions(){
+		return this.Transactions;
+	}
+	
+	public void addTransaction(Transaction transaction) {
+		Transactions.put(transaction.getID(),transaction);
+	}
+	
+	public void deleteTransaction(Transaction transaction) {
+		Transactions.remove(transaction.getID());
+	}
+	
+	
+}
