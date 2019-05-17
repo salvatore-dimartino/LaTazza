b3fa50d4aa13d925fb3db3d849ce8400a29d8e2a
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-17 10:45:20 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/BoxPurchase.java
@@ -0,0 +1,104 @@
+package it.polito.latazza.data;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class BoxPurchase extends Transaction {
+	
+	Integer quantity;
+	Beverage beverage;
+	
+	public BoxPurchase(Integer ID, Date date, Integer quantity, Beverage beverage) {
+		
+		super(ID, date);
+		this.quantity = quantity;
+		this.beverage = beverage;
+
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Beverage getBeverage() {
+		return beverage;
+	}
+
+	public void setBeverage(Beverage beverage) {
+		this.beverage = beverage;
+	}
+	
+	@Override
+	public String getString() {
+		return  this.getDate().toString() + " " + "BUY " + beverage.getName() + " " + quantity.toString();
+	}
+	
+	public List<String> getAttributes() {
+		
+		List<String> att = new ArrayList<String>();
+		
+		att.add(this.getDate().toString());
+		att.add(this.quantity.toString());
+		att.add(this.beverage.getID().toString());
+		
+		return att;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void toJsonTransaction() {
+		
+		JSONParser parser = new JSONParser();
+		JSONArray j_file = new JSONArray();
+		
+		File myfile = new File("Transactions.json");
+		try {
+			myfile.createNewFile();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		
+		try {
+			j_file = (JSONArray) parser.parse(new FileReader("./Transactions.json"));
+						
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (ParseException e) {
+		}	
+		
+		JSONObject boxpurchaseObject = new JSONObject();
+		boxpurchaseObject.put("ID", this.getID().toString());
+		boxpurchaseObject.put("List_Attributes", this.getAttributes());
+		boxpurchaseObject.put("Type", "BOXPURCHASE");
+		j_file.add(boxpurchaseObject);
+						
+		// write the json object to the file
+		try (FileWriter file = new FileWriter("./Transactions.json")) {
+			file.write(j_file.toJSONString());
+			file.flush();
+			file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+	    }
+
+	}
+
+
+	
+}
