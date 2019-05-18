2aab1176afaeb2afc7a1d385465c3a3a4adc62a5
Salvatore Di Martino <s218584@studenti.polito.it>
2019-05-18 21:54:13 +0000

    Update DataImplTest.java

--- src/test/java/it/polito/latazza/data/DataImplTest.java
+++ src/test/java/it/polito/latazza/data/DataImplTest.java
@@ -3,7 +3,12 @@ package it.polito.latazza.data;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import it.polito.latazza.exceptions.BeverageException;
+import it.polito.latazza.exceptions.DateException;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.sql.Date;
+
 import it.polito.latazza.exceptions.EmployeeException;
 import it.polito.latazza.exceptions.NotEnoughBalance;
 import it.polito.latazza.exceptions.NotEnoughCapsules;
@@ -52,9 +57,6 @@ public class DataImplTest extends junit.framework.TestCase {
 		assertTrue(data1.sellCapsules(0, 0, 30, true) == (20000 - 3000*3));
 		data1.reset();
 		this.setUp();
-		assertTrue(data1.sellCapsules(0, 0, 0, true) == (20000));
-		data1.reset();
-		this.setUp();
 		assertThrows(NotEnoughCapsules.class, () -> data1.sellCapsules(0, 0, 40, true));
 		data1.reset();
 		this.setUp();
@@ -75,49 +77,229 @@ public class DataImplTest extends junit.framework.TestCase {
 		this.setUp();
 		assertThrows(EmployeeException.class, () -> data1.sellCapsules(4, null, 40, true));
 		
-		//assertThrows(BeverageException.class, () -> data1.sellCapsules(1, 3, 10, true));
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testSellCapsulesToVisitor() {
+	public void testSellCapsulesToVisitor() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		try{
+			data1.sellCapsulesToVisitor(0, 10);
+			}
+		catch(BeverageException be) {
+			fail();
+		} catch (NotEnoughCapsules ce) {
+			fail();
+		}
+		data1.reset();
+		this.setUp();
+		assertThrows(NotEnoughCapsules.class, () -> data1.sellCapsulesToVisitor(0, 40));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.sellCapsulesToVisitor(3, 10));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.sellCapsulesToVisitor(null, 40));
+		
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testRechargeAccount() {
+	public void testRechargeAccount() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp(); //Personal is € 200 because you have not sold to the employee yet
+		assertEquals(data1.rechargeAccount(0, -10000).intValue(), 20000);
+		data1.reset();
+		this.setUp();
+		assertTrue(data1.rechargeAccount(0, 10000) == 30000);
+		data1.reset();
+		this.setUp(); //--> Should be EMPLOYEEEXCPETION
+		assertThrows(EmployeeException.class, () -> data1.rechargeAccount(3, -10000));
+		data1.reset();
+		this.setUp(); //--> Should be EMPLOYEEEXCPETION
+		assertThrows(EmployeeException.class, () -> data1.rechargeAccount(4, 30000));
+		
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testBuyBoxes() {
+	public void testBuyBoxes() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp(); 
+		assertThrows(BeverageException.class, () -> data1.buyBoxes(0, -1));
+		data1.reset();
+		this.setUp();
+		try {
+			data1.buyBoxes(0, 1);
+		}catch(BeverageException be) {
+			fail();
+		}
+		data1.reset();
+		this.setUp(); 
+		assertThrows(BeverageException.class, () -> data1.buyBoxes(null, -1));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.buyBoxes(null, 1));
+		
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Test
-	public void testGetEmployeeReport() {
+	public void testGetEmployeeReport() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		
+		Date d1 = new Date(2010, 1, 1);
+		Date d2 = new Date(2020, 1, 1);
+		try{
+			data1.getEmployeeReport(0, d1, d2);
+		} catch (EmployeeException ee) {
+			fail();
+		}
+		catch (DateException de) {
+			fail();
+		}
+		
+		data1.reset();
+		this.setUp();
+		assertThrows(DateException.class, () -> data1.getEmployeeReport(0, d2, d1));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.getEmployeeReport(5, d2, d1));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.getEmployeeReport(null, d2, d1));
+		
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testGetReport() {
+	public void testGetReport() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		
+		Date d1 = new Date(2010, 1, 1);
+		Date d2 = new Date(2020, 1, 1);
+		try{
+			data1.getReport(d1, d2);
+		} catch (DateException de) {
+			fail();
+		}
+		
+		data1.reset();
+		this.setUp();
+		assertThrows(DateException.class, () -> data1.getReport(d2, d1));
+		
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testCreateBeverage() {
+	public void testCreateBeverage() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("@cqua", -10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("@cqua", -10, 8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("@cqua", 10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("@cqua", 10, 8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("Acqua", -10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("Acqua", -10, 8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.createBeverage("Acqua", 10, -8));
+		data1.reset();
+		this.setUp();
+		assertEquals(data1.createBeverage("Acqua", 10, 8).intValue(), 1);
+		
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testUpdateBeverage() {
+	public void testUpdateBeverage() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", -10, -8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", -10, 8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", 10, -8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", 10, 8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", -10, -8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", -10, 8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", 10, -8));
+		data1.reset();
+		this.setUp();
+		data1.createBeverage("Acqua", 10, 8);
+		try{
+			data1.updateBeverage(1, "Acqua", 10, 8);
+		}catch(BeverageException be) {
+			fail();
+		}
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", -10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", -10, 8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", 10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "@cqua", 10, 8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", -10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", -10, 8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", 10, -8));
+		data1.reset();
+		this.setUp();
+		assertThrows(BeverageException.class, () -> data1.updateBeverage(1, "Acqua", 10, -8));
 		// TODO Auto-generated method stub
 		
 	}
@@ -159,13 +341,61 @@ public class DataImplTest extends junit.framework.TestCase {
 	}
 
 	@Test
-	public void testCreateEmployee() {
+	public void testCreateEmployee() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.createEmployee("Ferdinan0", "Be11o"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.createEmployee("Ferdinan0", "Bello"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.createEmployee("Ferdinano", "Be11o"));
+		data1.reset();
+		this.setUp();
+		try {
+			data1.createEmployee("Ferdinano", "Bello");
+		} catch (EmployeeException e) { 
+			fail();
+		}
+		
 		// TODO Auto-generated method stub
 		return;
 	}
 
 	@Test
-	public void testUpdateEmployee() {
+	public void testUpdateEmployee() throws EmployeeException, BeverageException, NotEnoughBalance {
+		
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(1, "Ferdinan0", "Be11o"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(1, "Ferdinan0", "Bello"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(1, "Ferdinano", "Be11o"));
+		data1.reset();
+		this.setUp();
+		try {
+			data1.updateEmployee(1, "Ferdinano", "Bello");
+		} catch (EmployeeException e) { 
+			fail();
+		}
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(4, "Ferdinan0", "Be11o"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(3, "Ferdinan0", "Bello"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(null, "Ferdinano", "Be11o"));
+		data1.reset();
+		this.setUp();
+		assertThrows(EmployeeException.class, () -> data1.updateEmployee(null, "Ferdinano", "Bello"));
+		
 		// TODO Auto-generated method stub
 		return;
 	}
