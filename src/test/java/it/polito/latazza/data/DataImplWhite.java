73661dd6491b07ceb6fae59d366b82cfc25239f9
Federico Silvio Gorrino <s262948@studenti.polito.it>
2019-05-18 20:39:00 +0000

    Upload New File

---
+++ src/test/java/it/polito/latazza/data/DataImplWhite.java
@@ -0,0 +1,327 @@
+package it.polito.latazza.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import it.polito.latazza.exceptions.BeverageException;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.beans.Transient;
+
+import it.polito.latazza.exceptions.EmployeeException;
+import it.polito.latazza.exceptions.NotEnoughBalance;
+import it.polito.latazza.exceptions.NotEnoughCapsules;
+
+public class DataImplTest extends junit.framework.TestCase {
+
+    DataImpl data1 = new DataImp();
+	Employee e1, e2, e3;
+	Beverage b1, b2, b3;
+
+    @Test
+    public void testDataImpl() {
+		
+		return;
+    }
+
+    @Test
+    public void testSellCapsules() throws EmployeeException, BeverageException, NotEnoughCapsules{
+        data1.reset();
+        try{
+            assertThrows(EmployeeException.class, () -> data1.sellCapsules(0,0,10,false));
+            data1.createEmployee("Massimo","Priano");
+            assertThrows(BeverageException.class, () -> data1.sellCapsules(0,0,10,false));
+            data1.createBeverage("Caffe",10,100);
+            assertThrows(NotEnoughCapsules.class, () -> data1.sellCapsules(0,0,10,false));
+            data1.rechargeAccount(0,1000);
+            data1.buyBoxes(0,2);
+            assertTrue(data1.sellCapsules(0,0,0,false)==1000);
+            assertTrue(data1.sellCapsules(0,0,10,false)==1000);
+            assertTrue(data1.sellCapsules(0,0,10,true)==900);
+        } catch (Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testSellCapsulesToVisitor() throws BeverageException, NotEnoughCapsules{
+        data1.reset();
+        try{
+            assertThrows(BeverageException.class, () -> data1.sellCapsulesToVisitor(0,10));
+            data1.createBeverage("Caffe",10,100);
+            assertThrows(NotEnoughCapsules.class, () -> data1.sellCapsulesToVisitor(0,10));
+            data1.rechargeAccount(0,1000);
+            data1.buyBoxes(0,2);
+            data1.sellCapsulesToVisitor(0,0);
+            data1.sellCapsulesToVisitor(0,10);
+        } catch (Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testRechargeAccount() throws EmployeeException{
+        data1.reset();
+        try{
+            assertThrows(EmployeeException.class, () -> data1.rechargeAccount(0,1000));
+            data1.createEmployee("Massimo","Priano");
+            assertTrue(data1.rechargeAccount(0,0)==0);
+            assertTrue(data1.rechargeAccount(0,1000)==1000);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testBuyBoxes() throws BeverageException, NotEnoughBalance{
+        data1.reset();
+        try{
+            assertThrows(BeverageException.class, () -> data1.buyBoxes(0,2));
+            data1.createBeverage("Caffe",10,100);
+            assertThrows(NotEnoughBalance.class, () -> data1.buyBoxes(0,2));
+            data1.rechargeAccount(0,1000);
+            assertTrue(data1.buyBoxes(0,2)==800);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetEmployeeReport() throws EmployeeException, DateException{
+        data1.reset();
+        Data start = new Date();
+        Data end = new Date();
+        try{
+            assertThrows(EmployeeException.class, () -> data1.getEmployeeReport(0,start,end));
+            data1.createEmployee("Massimo","Priano");
+            assertThrows(DateException.class, () -> data1.getEmployeeReport(0,null,end));
+            assertThrows(DateException.class, () -> data1.getEmployeeReport(0,start,null));
+            assertThrows(DateException.class, () -> data1.getEmployeeReport(0,end,start));
+            assertTrue(data1.getEmployeeReport(0,start,end).size()==0);
+            data1.rechargeAccount(0,1000);
+            assertTrue(data1.getEmployeeReport(0,start,end).size()==1);
+            data1.rechargeAccount(0,1000);
+            assertTrue(data1.getEmployeeReport(0,start,end).size()==2);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetReport() throws DataException{
+        data1.reset();
+        Data start = new Date();
+        Data end = new Date();
+        try{
+            data1.createEmployee("Massimo","Priano");
+            assertThrows(DateException.class, () -> data1.getReport(null,end));
+            assertThrows(DateException.class, () -> data1.getReport(start,null));
+            assertThrows(DateException.class, () -> data1.getReport(end,start));
+            assertTrue(data1.getReport(0,start,end).size()==0);
+            data1.rechargeAccount(0,1000);
+            assertTrue(data1.getReport(0,start,end).size()==1);
+            data1.rechargeAccount(0,1000);
+            assertTrue(data1.getReport(0,start,end).size()==2);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testCreateBeverage() throws BeverageException{
+        data1.reset();
+        try{
+            assertThrows(BeverageException.class, () -> data1.createBeverage(0,null,10,100));
+            data1.createBeverage("Caffe",10,100);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testUpdateBeverage() throws BeverageException{
+        data1.reset();
+        try{
+            assertThrows(BeverageException.class, () -> data1.updateBeverage(0,"Caffe",10,100));
+            data1.createBeverage("Caffe",10,100);
+            data1.updateBeverage(0,"The",5,50);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+    
+    @Test
+    public void testGetBeverageName() throws BeverageException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getBeverageName(0));
+            data1.createBeverage("Caffe",10,100);
+            assertTrue(data1.getBeverageName(0).equals("Caffe"));
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetBeverageCapsulesPerBox() throws BeverageException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getBeverageCapsulesPerBox(0));
+            data1.createBeverage("Caffe",10,100);
+            assertTrue(data1.getBeverageCapsulesPerBox(0)==10);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetBeverageBoxPrice() throws BeverageException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getBeverageBoxPrice(0));
+            data1.createBeverage("Caffe",10,100);
+            assertTrue(data1.getBeverageBoxPrice(0)==100);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetBeveragesId(){
+        data1.reset();
+        try{
+            assertTrue(data1.getBeveragesId().size() == 0);
+            data1.createBeverage("Caffe",10,100);
+            assertTrue(data1.getBeveragesId().size() == 1);
+            data1.createBeverage("The",5,50);
+            assertTrue(data1.getBeveragesId().size() == 2);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetBeverages(){
+        data1.reset();
+        try{
+            assertTrue(data1.getBeverages().size() == 0);
+            data1.createBeverage("Caffe",10,100);
+            assertTrue(data1.getBeverages().size() == 1);
+            data1.createBeverage("The",5,50);
+            assertTrue(data1.getBeverages().size() == 2);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetBeverageCapsules() throws BeverageException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getBeverageCapsules(0));
+            data1.createBeverage("Caffe",10,100);
+            assertTrue(data1.getBeverageCapsules(0)==0);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testCreateEmployee() throws EmployeeException{
+        data1.reset();
+        try{
+            assertThrows(EmployeeException.class, () -> data1.createEmployee(0,null,"Caldarone"));
+            assertThrows(EmployeeException.class, () -> data1.createEmployee(0,"Massimo",null));
+            data1.createEmployee("Fausto","Repetto");
+        } catch (EmployeeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testUpdateEmployee() throws EmployeeException{
+        data1.reset();
+        try{
+            assertThrows(EmployeeException.class, () -> data1.updateEmployee(0,"Massimo","Caldarone"));
+            data1.createEmployee("Fausto","Repetto");
+            data1.updateEmployee(0,"Massimo","Priano");
+        } catch (EmployeeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetEmployeeName() throws EmployeeException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getEmployeeName(0));
+            data1.createEmployee("Massimo","Priano");
+            assertTrue(data1.getEmployeeName(0).equals("Massimo"));
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetEmployeeSurname() throws EmployeeException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getEmployeeSurname(0));
+            data1.createEmployee("Massimo","Priano");
+            assertTrue(data1.getEmployeeSurname(0).equals("Priano"));
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetEmployeeBalance() throws EmployeeException {
+        data1.reset();
+        try{
+            assertThrows(EmployeeException, () -> data1.getEmployeeBalance(0));
+            data1.createEmployee("Massimo","Priano");
+            assertTrue(data1.getEmployeeBalance(0)==0);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetEmployeesId(){
+        data1.reset();
+        try{
+            assertTrue(data1.getEmployeesId().size() == 0);
+            data1.createEmployee("Fausto","Repetto");
+            assertTrue(data1.getEmployeesId().size() == 1);
+            data1.createEmployee("Massimo","Caldarone");
+            assertTrue(data1.getEmployeesId().size() == 2);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetEmployees(){
+        data1.reset();
+        try{
+            assertTrue(data1.getEmployees().size() == 0);
+            data1.createEmployee("Fausto","Repetto");
+            assertTrue(data1.getEmployees().size() == 1);
+            data1.createEmployee("Massimo","Caldarone");
+            assertTrue(data1.getEmployees().size() == 2);
+        } catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetBalance(){
+        data1.reset();
+        try{
+            data1.createBeverage("Massimo","Priano");
+            data1.rechargeAccount(0,1000);
+            assertTrue(data1.getBalance()==1000);
+        } catch(Exception e){
+            fail();
+        }
+    }
+}
