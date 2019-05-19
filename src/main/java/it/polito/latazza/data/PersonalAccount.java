a54f2c5b7ccb78503a7d3f9f61b7d5c0db8bafef
Salvatore Di Martino <s218584@studenti.polito.it>
2019-05-19 20:03:41 +0000

    Upload New File

---
+++ src/main/java/it/polito/latazza/data/PersonalAccount.java
@@ -0,0 +1,43 @@
+package it.polito.latazza.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import it.polito.latazza.exceptions.NotEnoughBalance;
+
+public class PersonalAccount {
+	private Integer balance;
+	private Map<Integer,Transaction> Transactions=new HashMap<Integer,Transaction>();
+	
+	public PersonalAccount(Integer balance) throws NotEnoughBalance {
+		if(balance >= 0)
+			this.balance=balance;
+		else
+		throw new NotEnoughBalance();
+	}
+	
+	public Integer getBalance() {
+		return this.balance;
+	}
+	
+	public void setBalance(Integer balance) throws NotEnoughBalance {
+		if(balance >= 0)
+			this.balance=balance;
+		else
+		throw new NotEnoughBalance();
+	}
+	
+	public Map<Integer,Transaction> getTransactions(){
+		return this.Transactions;
+	}
+	
+	public void addTransaction(Transaction transaction) {
+		Transactions.put(transaction.getID(),transaction);
+	}
+	
+	public void deleteTransaction(Transaction transaction) {
+		Transactions.remove(transaction.getID());
+	}
+	
+	
+}
