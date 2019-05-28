3ab5418f90582086893574fe79f857c5f1af54b1
Francesco Dibitonto <s265421@studenti.polito.it>
2019-05-28 10:51:28 +0000

    Update DataImpl.java

--- src/main/java/it/polito/latazza/data/DataImpl.java
+++ src/main/java/it/polito/latazza/data/DataImpl.java
@@ -385,7 +385,7 @@ public class DataImpl implements DataInterface {
 
 	@Override
 	public void updateEmployee(Integer id, String name, String surname) throws EmployeeException {
-		if(id==null || name==null || surname==null || Employees.get(id) == null) {
+		if(id==null || name==null || surname==null || Employees.get(id) == null || name.isEmpty() || surname.isEmpty()) {
 			throw new EmployeeException();
 		} else {
 			Employees.get(id).setName(name);
